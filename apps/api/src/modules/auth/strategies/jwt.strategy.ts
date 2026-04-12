37d46f4308a23515ba3671f2fb3ab87afa5f26e5
Brandonsxchevy <brandonsxchevy@yahoo.com>
2026-04-11 19:05:00 -0700

    Update jwt.strategy.ts

--- apps/api/src/modules/auth/strategies/jwt.strategy.ts
+++ apps/api/src/modules/auth/strategies/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: config.get('JWT_SECRET'),
+      secretOrKey: config.get('JWT_SECRET') || 'xnKJz61BBE8AZTECFzg3WLhl',
     });
   }
 
