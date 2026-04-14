42a7acb977f80f7fa36ea2f9fbdfea4d29266bb0
Brandonsxchevy <brandonsxchevy@yahoo.com>
2026-04-13 23:57:38 -0700

    Update store.ts
    add admin

--- apps/web/src/lib/store.ts
+++ apps/web/src/lib/store.ts
@@ -5,7 +5,7 @@ interface User {
   id: string
   name: string
   email: string
-  role: 'USER' | 'DEVELOPER'
+  role: 'USER' | 'DEVELOPER' | 'ADMIN'
   badges: string[]
   avgRating: number | null
   preferredLanguage: string
