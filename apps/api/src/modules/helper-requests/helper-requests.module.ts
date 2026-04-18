37f5a0de19d4cd960df27b4682a48f43552315c0
Brandonsxchevy <brandonsxchevy@yahoo.com>
2026-04-18 11:27:45 -0700

    Update helper-requests.module.ts
    helper

--- apps/api/src/modules/helper-requests/helper-requests.module.ts
+++ apps/api/src/modules/helper-requests/helper-requests.module.ts
@@ -1,1 +1,12 @@
+import { Module } from '@nestjs/common';
+import { HelperRequestsService } from './helper-requests.service';
+import { HelperRequestsController } from './helper-requests.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
+@Module({
+  imports: [PrismaModule],
+  controllers: [HelperRequestsController],
+  providers: [HelperRequestsService],
+  exports: [HelperRequestsService],
+})
+export class HelperRequestsModule {}
