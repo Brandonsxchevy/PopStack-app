429b05dd5ea52e8618e7478c413e3c4b6f8c28f9
Brandonsxchevy <brandonsxchevy@yahoo.com>
2026-04-18 11:29:37 -0700

    Update helper-requests.module.ts
    remove, prisma not used

--- apps/api/src/modules/helper-requests/helper-requests.module.ts
+++ apps/api/src/modules/helper-requests/helper-requests.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HelperRequestsService } from './helper-requests.service';
 import { HelperRequestsController } from './helper-requests.controller';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [],
   controllers: [HelperRequestsController],
   providers: [HelperRequestsService],
   exports: [HelperRequestsService],
