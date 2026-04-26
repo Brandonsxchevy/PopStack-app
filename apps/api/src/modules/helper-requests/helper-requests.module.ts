9d984303fa85169390b84da9d3e984e39372c557
Brandonsxchevy <brandonsxchevy@yahoo.com>
2026-04-26 13:18:59 -0700

    Update helper-requests.module.ts
    add helper payment to mod

--- apps/api/src/modules/helper-requests/helper-requests.module.ts
+++ apps/api/src/modules/helper-requests/helper-requests.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HelperRequestsService } from './helper-requests.service';
 import { HelperRequestsController } from './helper-requests.controller';
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, PaymentsModule],
   controllers: [HelperRequestsController],
   providers: [HelperRequestsService],
   exports: [HelperRequestsService],
